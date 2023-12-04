4d76aa6554b1183bad1fa7c8caf8d50a6b723f55
saroj-mali <sarojmali6090@gmail.com>
2023-12-04 19:59:39 +0530

    work pending

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { store } from "./Store";
 import { BsCartFill } from "react-icons/bs";
 import CartItem from "./CartItem";
 import { BsCurrencyRupee } from "react-icons/bs";
